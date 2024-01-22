9e47b0a10b1b7c7ed369bb99806e0e7d570e24bd
Thalison <48662619+Thalison-ms@users.noreply.github.com>
2024-01-22 00:36:16 -0400

    Create novo.py

---
+++ novo.py
@@ -0,0 +1,22 @@
+####### OPERADORES / TIPOS / ESTRUTURAS DE DADOS
+"""
+    +   Soma
+    -   Subtração
+    *   Multiplicação
+    /   Divisão
+    **  Potência
+    %   Módulo
+"""
+a = "teste"
+print(type(1))
+print(4+4.0)
+print(4//2)
+print(4/2)
+
+print("-"*50)
+a = 6
+b = float(a)
+print(a)
+print(b)
+
+print("-"*50)
