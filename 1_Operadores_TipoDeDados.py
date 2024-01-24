407a72edce65bcb0a76ef74da78d6af9e80efdc7
Thalison <48662619+Thalison-ms@users.noreply.github.com>
2024-01-23 22:41:09 -0400

    Update and rename novo.py to 1_Operadores_TipoDeDados.py

---
+++ 1_Operadores_TipoDeDados.py
@@ -0,0 +1,32 @@
+####### OPERADORES / TIPOS DE DADOS
+
+import os
+
+"""
+    +   Soma
+    -   Subtração
+    *   Multiplicação
+    /   Divisão
+    **  Potência
+    %   Módulo
+"""
+os.system('cls') 
+
+a = "teste"
+print(type(1))
+print(4+4.0)
+print(4//2)
+print(4/2)
+
+print("-"*50)
+print("CONVERSAO")
+a = 6
+b = float(a)
+print(a)
+print(b)
+
+print("-"*50)
+print("FUNCOES ABS, ROUND, POW")
+print(abs(-8))                      # Transforma um número negativo em positivo
+print(round(3.141416,2))            # Arredonda com número de casas desejado
+print(pow(4,2))                     # Realiza a potência, número/potência
