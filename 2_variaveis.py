9b5c3b9bb00a180e299021d1599b75985f25c5cc
Thalison <48662619+Thalison-ms@users.noreply.github.com>
2024-01-23 23:10:48 -0400

    Create 2_variaveis.py

---
+++ 2_variaveis.py
@@ -0,0 +1,29 @@
+####### VARIÁVEIS
+import os
+os.system('cls') 
+
+
+# Variáveis em Python tem tipagem Dinâmica, é bom e ruim, em determinadas validações, caso não preste atenção nessa informação quebra o código
+
+print("-"*50)
+var_teste = 1
+print(var_teste)
+print(type(var_teste))
+
+print("-"*50)
+var_teste = "Thalison"
+print(var_teste)
+print(type(var_teste))
+
+print("-"*50)
+print("Declaração Multipla")
+pessoa1, pessoa2, pessoa3 = "Bob", "Maria", "Ana"
+print(pessoa1)
+print(pessoa2)
+print(pessoa3)
+fruta1 = fruta2 = fruta3 = "Melancia"
+print(fruta1)
+print(fruta2)
+print(fruta3)
+
+print("-"*50)
